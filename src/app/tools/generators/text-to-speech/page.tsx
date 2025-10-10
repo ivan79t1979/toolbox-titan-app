82646ffa27699ca13d5ddbc26e34d4fcb7657a27
Ivan Kováč <Timemaster1979@gmail.com>
2025-10-10 17:54:26 +0000

    Can we add more languages to our app?

--- src/app/tools/generators/text-to-speech/page.tsx
+++ src/app/tools/generators/text-to-speech/page.tsx
@@ -1,25 +1,14 @@
-'use client';
-
 import { PageHeader } from '@/components/page-header';
-import { GradioWrapper } from '@/components/gradio-wrapper';
-import { useEffect, useState } from 'react';
+import { TextToSpeechForm } from './text-to-speech-form';
 
 export default function TextToSpeechPage() {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
   return (
     <>
       <PageHeader
         title="Text to Speech"
-        description="Convert text to spoken audio."
+        description="Convert text to spoken audio with a variety of voices."
       />
-      <div className="mt-8">
-        {isClient ? <GradioWrapper appSrc="https://timemaster-multilingual-tts.hf.space" scriptSrc="https://gradio.s3-us-west-2.amazonaws.com/5.35.0/gradio.js" /> : <div>Loading Tool...</div>}
-      </div>
+      <TextToSpeechForm />
     </>
   );
 }
