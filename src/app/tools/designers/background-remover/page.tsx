0b95bb1fe674e6f20033aed4bcaba68d91e6735f
Ivan Kováč <Timemaster1979@gmail.com>
2026-03-03 09:48:04 +0000

    fixed sime text

--- src/app/tools/designers/background-remover/page.tsx
+++ src/app/tools/designers/background-remover/page.tsx
@@ -53,7 +53,8 @@ export default function BackgroundRemoverPage() {
       />
       <PageHeader
         title="Background Remover"
-        description="Removes the background from an image." /n "If the tool doeasn't load reload the page"
+        description="Removes the background from an image.
+        Note: Refresh the page if the tool doesn't load."
       />
       <div className="mt-8">
         <GradioWrapper appSrc="https://timemaster-background-remover-c1.hf.space" scriptSrc="https://gradio.s3-us-west-2.amazonaws.com/5.35.0/gradio.js" />
